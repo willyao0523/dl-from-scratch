c0e8face62a1b1fb847587475324e38dc5d618fc
wyao <willjarvin@gmail.com>
2023-07-18 20:17:58 +0800

    feat: add backprop

---
+++ backpropagation/buy_apple.py
@@ -0,0 +1,20 @@
+from naive_layer import *
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print("price:", int(price))
+print("dApple:", dapple)
+print("dApple_num:", int(dapple_num))
+print("dTax:", dtax)
